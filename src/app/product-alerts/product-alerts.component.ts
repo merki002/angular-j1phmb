d9a8d1254d9c2fbd39a7f4b788f1e2914590dd6a
merki002 <merki002@morris.umn.edu>
2020-02-09 15:13:54 -0600

    Fix the broken template

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -7,7 +7,6 @@ import { Output, EventEmitter } from '@angular/core';
   templateUrl: './product-alerts.component.html',
   styleUrls: ['./product-alerts.component.css']
 })
-
 export class ProductAlertsComponent {
   @Input() product;
   @Output() notify = new EventEmitter();
