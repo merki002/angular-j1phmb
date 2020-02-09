38cd26af6d1b68b91d3bef7106956660ea0bfff1
merki002 <merki002@morris.umn.edu>
2020-02-09 15:33:20 -0600

    modify import component and on init method in product details

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
+import { products } from '../products';
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
   styleUrls: ['./product-details.component.css']
 })
 export class ProductDetailsComponent implements OnInit {
+  product;
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+  constructor(
+    private route: ActivatedRoute,
+  ) { }
 
 }
