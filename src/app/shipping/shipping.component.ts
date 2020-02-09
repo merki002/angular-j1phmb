c79af879d0ae089b4e97e812d6a5f89e0b34b6ae
merki002 <merki002@morris.umn.edu>
2020-02-09 15:59:06 -0600

    modify shipping to import cart service

--- src/app/shipping/shipping.component.ts
+++ src/app/shipping/shipping.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+import { CartService } from '../cart.service';
 @Component({
   selector: 'app-shipping',
   templateUrl: './shipping.component.html',
